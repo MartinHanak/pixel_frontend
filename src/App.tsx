20f61cd7e32c1c644164a11d7f6a744d9002a241
Martin Hanák <martinhanak97@gmail.com>
2023-03-10 06:58:55 +0100

    Initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Navbar />
       </header>
-    </div>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </main>
+
+      <footer></footer>
+    </>
   );
 }
 
