f85e623637ad0e7dc76b313778e2c554b552e397
Martin Hanák <martinhanak97@gmail.com>
2023-07-03 10:28:21 +0200

    Fix env files for production

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,1 @@
-const devMode = process.env.REACT_APP_DEVMODE;
-
-export const BACKEND_URL = devMode ? "http://localhost:3002" : "https://www.quizzillionaire.com";
+export const BACKEND_URL = "https://www.quizzillionaire.com";
